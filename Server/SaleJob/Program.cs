62966163aa962ccc7b503cbb6d8d75fb6ccd743a
YiTing <atake441@gmail.com>
2026-07-13 16:08:34 +0800

    新增固定彙整排程檔案

---
+++ Server/SaleJob/Program.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+
+
+var configuration = new ConfigurationBuilder()
+    .SetBasePath(AppContext.BaseDirectory)
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .Build();
+
+var services = new ServiceCollection();
+
+services.AddSingleton<IConfiguration>(configuration);
+
+// Repository 註冊
+services.AddScoped<ISaleSumRepository, SaleSumRepository>();
+services.AddScoped<ISaleRepository, SaleRepository>();
+
+// Service 註冊
+services.AddScoped<SaleSumService>();
+
+var serviceProvider = services.BuildServiceProvider();
+
+using var scope = serviceProvider.CreateScope();
+
+var saleSumService = scope.ServiceProvider.GetRequiredService<SaleSumService>();
+
+var request = new RequestBase
+{
+    StoreId = "123331",
+    //Date = DateOnly.Parse("2026-05-01")
+    Date = DateOnly.FromDateTime(DateTime.Today.AddDays(-1))
+};
+
+
+Console.WriteLine("排程開始！！！");
+
+var result = await saleSumService.Summary(request);
+
+Console.WriteLine("排程結束！！！" + result.Message);
